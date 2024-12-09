a4e87edcdaf7a68a689e9992f243e42c94486aa0
Tyler Bowers <tbowers@choctawnation.com>
2024-12-09 12:24:41 -0600

    removed import and put into package

--- Day7/Part1.scala
+++ Day7/Part1.scala
@@ -1,32 +1,35 @@
-import scala.collection.mutable.Stack
-@main
-def solution(inputPath: String): Unit = {
-  var answer: Long = 0
-  val lines = readLines(inputPath)
-  for line <- lines do {
-    val pattern = "\\d+".r
-    val numbers = pattern.findAllMatchIn(line).map(m => m.toString.toLong).toList
-    
-    val goal = numbers(0)
-    val results = possibilities(numbers.drop(2), numbers(1), List[Long]())
-    if (results.contains(goal)) answer += goal
+package Day7;
+
+object Part1 {
+  @main
+  def solution(inputPath: String): Unit = {
+    var answer: Long = 0
+    val lines = readLines(inputPath)
+    for line <- lines do {
+      val pattern = "\\d+".r
+      val numbers = pattern.findAllMatchIn(line).map(m => m.toString.toLong).toList
+      
+      val goal = numbers(0)
+      val results = possibilities(numbers.drop(2), numbers(1), List[Long]())
+      if (results.contains(goal)) answer += goal
+    }
+  
+    println(answer)
   }
-
-  println(answer)
-}
-
-def possibilities(operands: List[Long], total: Long, result: List[Long]): List[Long] = {
-  if (operands.isEmpty) {
-    return result :+ total
+  
+  def possibilities(operands: List[Long], total: Long, result: List[Long]): List[Long] = {
+    if (operands.isEmpty) {
+      return result :+ total
+    }
+    return possibilities(operands.drop(1), total + operands(0), result) ::: possibilities(operands.drop(1), total * operands(0), result)
   }
-  return possibilities(operands.drop(1), total + operands(0), result) ::: possibilities(operands.drop(1), total * operands(0), result)
-}
-
-def readLines(path: String): List[String] = {
-  val source = scala.io.Source.fromFile(path)
-  try {
-    return source.getLines().toList
-  } finally {
-    source.close()
+  
+  def readLines(path: String): List[String] = {
+    val source = scala.io.Source.fromFile(path)
+    try {
+      return source.getLines().toList
+    } finally {
+      source.close()
+    }
   }
 }
