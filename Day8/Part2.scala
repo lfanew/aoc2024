4fa849d08bea5bd5bc3eb4f34f4e6f67ff1c39fd
Tyler Bowers <tyler.bowers@outlook.com>
2024-12-09 20:44:12 -0600

    day 8 part 2 solution

---
+++ Day8/Part2.scala
@@ -0,0 +1,73 @@
+package Day8;
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+object Part2 {
+  @main
+  def solution(inputPath: String): Unit = {
+    val lines = readLines(inputPath)
+    val grid = Grid(lines(0).length(), lines.size)
+    for (line, y) <- lines.zipWithIndex do {
+      for (c, x) <- line.zipWithIndex do {
+        if (c != '.') {
+          val point = Point(x, y)
+          grid.antennas.get(c) match {
+            case Some(a) => a.append(point)
+            case None => {
+              grid.antennas(c) = ListBuffer(point)
+            }
+          }
+        }
+      }
+    }
+
+    val antinodes = mutable.Set[Point]()
+    for key <- grid.antennas.keySet do {
+      val points = grid.antennas(key)
+      val pairs = ListBuffer[(Point, Point)]()
+      for i <- 0 until points.size do
+        for j <- i + 1 until points.size do
+          val difference = points(i) - points(j)
+          var current = points(i)
+          while grid.contains(current) do
+            antinodes.add(current)
+            current = current + difference
+          current = points(j)
+          while grid.contains(current) do
+            antinodes.add(current)
+            current = current - difference
+    }
+    println(antinodes.size)
+  }
+
+  def readLines(path: String): List[String] = {
+    val source = scala.io.Source.fromFile(path)
+    try {
+      return source.getLines().toList
+    } finally {
+      source.close()
+    }
+  }
+
+  class Point(val x: Int, val y: Int) {
+    def +(other: Point): Point = Point(this.x + other.x, this.y + other.y)
+    def -(other: Point): Point = Point(this.x - other.x, this.y - other.y)
+
+    override def toString(): String = s"[$x,$y]"
+    override def equals(obj: Any): Boolean = {
+      obj match {
+        case that: Point => that.x == x && that.y == y
+        case _           => false
+      }
+    }
+    override def hashCode(): Int = 31 * x + y
+  }
+
+  class Grid(val width: Int, val height: Int) {
+    val antennas = mutable.Map[Char, ListBuffer[Point]]()
+    def contains(point: Point): Boolean = {
+      0.until(width).contains(point.x) && 0.until(height).contains(point.y)
+    }
+  }
+}
